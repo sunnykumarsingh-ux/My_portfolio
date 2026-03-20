5ea65588f7015e12bd3ad04caa262aef8df696a4
Sonic boy <sunnyk61460@gmail.com>
2026-03-20 20:32:44 +0530

    Remove Spline 3D viewer from Experience section

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -42,14 +42,3 @@ export type TMotion = {
   delay: number;
   duration: number;
 };
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "spline-viewer": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement> & { url?: string },
-        HTMLElement
-      >;
-    }
-  }
-}
